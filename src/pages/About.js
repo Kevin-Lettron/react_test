0e9e083c9f4d7d16a32c1a73e5d8495b9e36fe8c
Kevin-Lettron <101657367+Kevin-Lettron@users.noreply.github.com>
2022-04-28 11:23:47 +0200

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation /> 
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet ullam molestiae quam dolores labore iure eos voluptatem expedita quod laudantium temporibus, modi obcaecati provident eius quia, tenetur, ut maiores adipisci! Commodi, voluptate ducimus perspiciatis voluptates alias nihil, in doloremque praesentium odit accusantium dolorum quasi incidunt nam ad perferendis vero obcaecati molestias exercitationem ratione natus atque unde quia quis maxime! Quos nihil ad, animi laborum doloribus vitae, ea non dolorum beatae autem eius dolores libero perspiciatis. Ratione doloribus numquam ea tenetur atque! Nihil rem, tempore eligendi quis maxime voluptatum voluptatibus necessitatibus omnis totam natus in error non repellat dolor est. Deleniti.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus incidunt, nesciunt dicta similique ipsam maiores, corrupti fugit omnis ex voluptas cum totam nam modi, odit id! Quis impedit recusandae reprehenderit omnis illum itaque ad eius, dolore molestias nobis, voluptatum, vel quaerat enim officia! Animi voluptas debitis voluptatibus aspernatur iste soluta repudiandae alias itaque. Aperiam, similique fugit! Aperiam exercitationem rerum quia labore in nesciunt vel adipisci omnis recusandae ad fugiat debitis praesentium iure natus laudantium quasi, asperiores et modi perspiciatis aliquam earum consequatur culpa. Fugit dolore, fugiat quam recusandae deleniti dolorum cumque autem debitis assumenda excepturi itaque, sit architecto dolores alias doloribus illo. Aut sapiente obcaecati, vero provident ut, saepe dolorem delectus veniam dolor doloribus corporis exercitationem! Doloribus voluptatum excepturi cum beatae harum molestias! Non tempore aspernatur hic veritatis nihil, consequatur molestiae facere ullam autem quas corrupti maiores repellat ex officiis? Culpa iste ut minima repellendus molestias deserunt? Expedita, harum sunt. Fugiat, odit architecto eveniet tenetur nihil numquam dicta vitae exercitationem nemo corporis atque animi voluptate excepturi quae libero pariatur mollitia possimus in ducimus dolorem minima assumenda? Alias, libero. Ipsum, facilis? Molestias ex, nam quia tenetur doloribus nesciunt ea amet obcaecati id asperiores officiis deleniti in sed, rerum voluptates tempore veritatis!</p>
+        </div>
+    );
+};
+
+export default About;
